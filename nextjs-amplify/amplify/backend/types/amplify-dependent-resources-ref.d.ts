bc09bc15208a7d53de908ff83adb306b22e9db96
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-01-14 19:50:16 +0100

    Added GraphQl Api and DynamoDB integration

--- nextjs-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ nextjs-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,28 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextjsamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "function": {
+    "nextjsamplify4f440d7a": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamoamplifydev": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
